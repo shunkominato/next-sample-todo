c4b3ac8ad553ffc2a81c285a5b4d0c8c4a79e3c4
15951595 <shun19871001@gmail.com>
2021-03-31 22:58:56 +0900

    layout add

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Layout = ({children, title = 'default'}) => {
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-white font-mono bg-gray-800">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-1 justify-center items-center w-screen flex-col">
+        {children}
+      </main>
+      <footer className="w-full h-6 flex justify-center items-center text-gray-500 text-sm">
+        @Udemy 2021
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
